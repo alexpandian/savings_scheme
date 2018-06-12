003264db15d9eba9a19aa96cdf2793333be19479
Alex pandian <email2alexpandian@gmail.com>
2018-06-12 16:59:05 +0530

    interceptor adding

--- savings-scheme-admin/src/app/app.module.ts
+++ savings-scheme-admin/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { AuthorizationInterceptor } from './interceptors/authorization.interceptor';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,6 +10,8 @@ import { ContentComponent } from './content/content.component';
 
 import { RoutingModule } from './routing/routing.module';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+
+
 
 
 @NgModule({
@@ -19,9 +23,16 @@ import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RoutingModule
   ],
-  providers: [],
+  providers: [
+     {
+       provide : HTTP_INTERCEPTORS,
+       useClass : AuthorizationInterceptor,
+       multi : true
+     }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ savings-scheme-admin/src/app/interceptors/authorization.interceptor.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { 
+			HttpRequest,
+			HttpHandler,
+			HttpEvent,
+			HttpInterceptor
+	    } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class AuthorizationInterceptor implements HttpInterceptor {
+
+	constructor(){}
+
+	intercept( request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+
+		request = request.clone({
+			setHeaders : {
+				Authorization : `Bearer alexpandian`
+			}
+		});
+		return next.handle(request);
+
+	}
+}
